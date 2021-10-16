d2acbf09c9876de60c0202551a29d32387d8a985
rutujakardile1998 <59498764+rutujakardile1998@users.noreply.github.com>
2021-10-16 15:53:38 +0530

    Second Commit
    any secnd rand msg

--- Rscript.py
+++ Rscript.py
@@ -1,1 +1,2 @@
 print("Hiiii... There....")
+print("Its R.............")
--- cal.py
+++ cal.py
@@ -2,3 +2,8 @@ print("-----------------------------------")
 print("--------Calculator--------------")
 
 a = input("Enter no A")
+b = input("Enter no B")
+
+c = a+b
+
+print("Add is = ", c)
